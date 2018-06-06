36b0f4688bdd33ac2749404841b3b180bc112529
Hoffit <dehull_1@yahoo.com>
2018-06-06 15:23:31 -0700

    Cleaned up a few logic gaps, and added the ability for the user to replay the game as many times as they want.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
 'use strict';
 
+/**
+ * This program is created as a part of a training exercise. The program is a guessing game with subject
+ * matter related to me. Interacting/quizzing the user through prompts and alerts.
+ */
 //Get user's name
 var userName = prompt('What should I call you?');
-var numberOfQuestions = 0;
+userName = (userName===null ? 'user' : userName);
+console.log('User name is: '+userName);
 
-//User wants to play?
+//Determine if the user wants to play
+//Prompt user until valid entry provided
 var validAffirtmativeAnswers = ['yes', 'YES', 'y', 'Y'];
 var validNegativeAnswers = ['no', 'NO', 'n', 'N'];
 var playTheGameAnswer;
 var validUserEntry = false;
-var wantsToPlay = false;
-
-//Prompt user until valid entry provided
-while(!validUserEntry) {
-  playTheGameAnswer = prompt(userName+', do you want to play my guessing game?').toUpperCase();
+var wantsToPlay = true;
+while(!validUserEntry && wantsToPlay) {
+  playTheGameAnswer = prompt(userName+', do you want to play my guessing game?');
   console.log('The user entered: '+playTheGameAnswer);
   if(validAffirtmativeAnswers.includes(playTheGameAnswer)) {
     console.log('The user provided a valid affirmative response: '+playTheGameAnswer);
@@ -32,128 +36,162 @@ while(!validUserEntry) {
           'OR\n'+
           'Enter a valid negative answer:'+validNegativeAnswers);
   }
+}
 
-  //Let's play!
-  if(wantsToPlay) {
-    var correctGuess = false;
-    var playerWantsToTryAgain = true;
-    var correctCount = 0;
-    console.log('The user provided a valid response and wants to play');
-    alert('Ok '+userName+' - ready player one!');
+//Let's play! User can replay game as many times as desired due to playAgain attribute.
+var playAgain = true;
+//Next two attributes are used to track and share score with user at end.
+var numberOfQuestions;
+var correctCount;
+while(playAgain && wantsToPlay) {
+  numberOfQuestions = 0;
+  correctCount = 0;
+  var correctGuess = false;
+  var playerWantsToTryAgain = true;
+  alert('Ok '+userName+' - Ready Player One!');
 
-    //First Question: age = 46
-    numberOfQuestions++;
-    while(!correctGuess && playerWantsToTryAgain) {
-      console.log('Begin question 1: my age');
-      var myAge = 46;
-      var myAgeGuess = parseInt(prompt(userName+', can you guess my age correctly?'));
-      console.log('The user entered: '+myAgeGuess);
-      if(myAgeGuess === myAge) {
-        correctGuess = true;
-        correctCount++;
-        console.log('The user got it right!');
-        alert('You got it!');
-      }
-      else {
-        console.log('The user got it wrong!');
-        playerWantsToTryAgain = confirm('That is not it! Try again? (hint - mid 40s)');
-        if(!playerWantsToTryAgain) {
-          alert('Bye '+userName+'!');
-        }
+  //First Question: age = 46
+  numberOfQuestions++;
+  while(!correctGuess && playerWantsToTryAgain) {
+    console.log('Begin question 1: my age');
+    var myAge = 46;
+    var myAgeGuess = prompt(userName+', can you guess my age correctly?');
+    myAgeGuess = (myAgeGuess === null ? null : parseInt(myAgeGuess));
+    console.log('The user entered: '+myAgeGuess);
+    if(myAgeGuess !== null && myAgeGuess === myAge) {
+      correctGuess = true;
+      correctCount++;
+      console.log('The user got it right!');
+      alert('You got it!');
+    }
+    else if(myAgeGuess !== null) {
+      console.log('The user got it wrong!');
+      playerWantsToTryAgain = confirm('That is not it! Try again? (hint - mid 40s)');
+      if(!playerWantsToTryAgain) {
+        console.log('The user gave up on this question! Moving on.');
+        alert('Next question!');
       }
     }
-    //Second Question: favorite tv show is  = Rick and Morty
-    correctGuess = false;
-    playerWantsToTryAgain = true;
-    numberOfQuestions++;
-    while(!correctGuess && playerWantsToTryAgain) {
-      console.log('Begin question 2: my fav tv show');
-      var myFavoriteTVShow = 'Rick and Morty';
-      var myFavoriteTVShowGuess = prompt(userName+', can you guess my favorite animated tv program?');
-      console.log('The user entered: '+myFavoriteTVShowGuess);
-      if(myFavoriteTVShowGuess === myFavoriteTVShow) {
-        correctGuess = true;
-        correctCount++;
-        console.log('The user got it right!');
-        alert('You got it!');
-      }
-      else {
-        console.log('The user got it wrong!');
-        playerWantsToTryAgain = confirm('That is not it! Try again? (hint - one main character is played by Chris Parnell)');
-        if(!playerWantsToTryAgain) {
-          alert('Bye '+userName+'!');
-        }
-      }
-    }
-    //Third Question: favorite tv show episode is  = Pickle Rick
-    correctGuess = false;
-    playerWantsToTryAgain = true;
-    numberOfQuestions++;
-    while(!correctGuess && playerWantsToTryAgain) {
-      console.log('Begin question 3: my fav episode');
-      var myFavoriteEpisode = 'Pickle Rick';
-      var myFavoriteEpisodeGuess = prompt(userName+', can you guess my favorite episode in the Rick and Morty animated tv program?');
-      console.log('The user entered: '+myFavoriteEpisodeGuess);
-      if(myFavoriteEpisodeGuess === myFavoriteEpisode) {
-        correctGuess = true;
-        correctCount++;
-        console.log('The user got it right!');
-        alert('You got it!');
-      }
-      else {
-        console.log('The user got it wrong!');
-        playerWantsToTryAgain = confirm('That is not it! Try again? (hint - Rick is in a real pickle this time bro!)');
-        if(!playerWantsToTryAgain) {
-          alert('Bye '+userName+'!');
-        }
-      }
+    else {
+      playerWantsToTryAgain = false;
+      console.log('The user is skipping this question! Moving on.');
     }
   }
-  //Fourth Question: worst place I ever lived is  = OMAHA
-  correctGuess = false;
-  playerWantsToTryAgain = true;
+
+  //Second Question: favorite tv show is  = Rick and Morty
   numberOfQuestions++;
+  correctGuess = false;
+  while(!correctGuess && playerWantsToTryAgain) {
+    console.log('Begin question 2: my fav tv show');
+    var myFavoriteTVShow = 'Rick and Morty';
+    var myFavoriteTVShowGuess = prompt(userName+', can you guess my favorite animated tv program?');
+    console.log('The user entered: '+myFavoriteTVShowGuess);
+    if(myFavoriteTVShowGuess !== null && myFavoriteTVShowGuess === myFavoriteTVShow) {
+      correctGuess = true;
+      correctCount++;
+      console.log('The user got it right!');
+      alert('You got it!');
+    }
+    else if(myFavoriteTVShowGuess !== null) {
+      console.log('The user got it wrong!');
+      playerWantsToTryAgain = confirm('That is not it! Try again? (hint - one main character is played by Chris Parnell)');
+      if(!playerWantsToTryAgain) {
+        console.log('The user gave up on this question! Moving on.');
+        alert('Next question!');
+      }
+    }
+    else {
+      playerWantsToTryAgain = false;
+      console.log('The user is skipping this question! Moving on.');
+    }
+  }
+
+  //Third Question: favorite tv show episode is  = Pickle Rick
+  numberOfQuestions++;
+  correctGuess = false;
+  while(!correctGuess && playerWantsToTryAgain) {
+    console.log('Begin question 3: my fav episode');
+    var myFavoriteEpisode = 'Pickle Rick';
+    var myFavoriteEpisodeGuess = prompt(userName+', can you guess my favorite episode in the Rick and Morty animated tv program?');
+    console.log('The user entered: '+myFavoriteEpisodeGuess);
+    if(myFavoriteEpisodeGuess !== null && myFavoriteEpisodeGuess === myFavoriteEpisode) {
+      correctGuess = true;
+      correctCount++;
+      console.log('The user got it right!');
+      alert('You got it!');
+    }
+    else if(myFavoriteEpisodeGuess !== null) {
+      console.log('The user got it wrong!');
+      playerWantsToTryAgain = confirm('That is not it! Try again? (hint - Rick is in a real pickle this time bro!)');
+      if(!playerWantsToTryAgain) {
+        console.log('The user gave up on this question! Moving on.');
+        alert('Next question!');
+      }
+    }
+    else {
+      playerWantsToTryAgain = false;
+      console.log('The user is skipping this question! Moving on.');
+    }
+  }
+
+  //Fourth Question: worst place I ever lived is  = OMAHA
+  numberOfQuestions++;
+  correctGuess = false;
   while(!correctGuess && playerWantsToTryAgain) {
     console.log('Begin question 4: my least favorite place to live');
     var myLeaseFavoriteCity = 'Omaha';
     var myLeaseFavoriteCityGuess = prompt(userName+', can you guess my least favorite place to live?');
     console.log('The user entered: '+myLeaseFavoriteCityGuess);
-    if(myLeaseFavoriteCityGuess === myLeaseFavoriteCity) {
+    if(myLeaseFavoriteCityGuess !== null && myLeaseFavoriteCityGuess === myLeaseFavoriteCity) {
       correctGuess = true;
       correctCount++;
       console.log('The user got it right!');
       alert('You got it!');
     }
-    else {
+    else if(myLeaseFavoriteCityGuess !== null) {
       console.log('The user got it wrong!');
       playerWantsToTryAgain = confirm('That is not it! Try again? (hint - it is in Nebraska)');
       if(!playerWantsToTryAgain) {
-        alert('Bye '+userName+'!');
+        console.log('The user gave up on this question! Moving on.');
+        alert('Next question!');
       }
     }
+    else {
+      playerWantsToTryAgain = false;
+      console.log('The user is skipping this question! Moving on.');
+    }
   }
+  
   //Fifth Question: best place I ever lived is  = Auckland
-  correctGuess = false;
-  playerWantsToTryAgain = true;
   numberOfQuestions++;
+  correctGuess = false;
   while(!correctGuess && playerWantsToTryAgain) {
-    console.log('Begin question 4: my  favorite place to live');
+    console.log('Begin question 5: my favorite place to live');
     var myFavoriteCity = 'Auckland';
     var myFavoriteCityGuess = prompt(userName+', can you guess my favorite place to live?');
     console.log('The user entered: '+myFavoriteCityGuess);
-    if(myFavoriteCityGuess === myFavoriteCity) {
+    if(myFavoriteCityGuess !== null && myFavoriteCityGuess === myFavoriteCity) {
       correctGuess = true;
       correctCount++;
       console.log('The user got it right!');
       alert('You got it!');
     }
-    else {
+    else if(myFavoriteCityGuess !== null) {
       console.log('The user got it wrong!');
       playerWantsToTryAgain = confirm('That is not it! Try again? (hint - it is the capital of New Zealand!)');
       if(!playerWantsToTryAgain) {
-        alert('Bye '+userName+'!');
+        console.log('The user gave up on this question! Moving on.');
+        alert('Next question!');
       }
     }
+    else {
+      playerWantsToTryAgain = false;
+      console.log('The user is skipping this question! Moving on.');
+    }
   }
+
+  //Let the user know how they did, and see if they want to try again
   alert('You got '+correctCount+' out of '+numberOfQuestions);
+  playAgain = confirm(userName+' Play again?');
 }
+alert('Bye '+userName+'!');
